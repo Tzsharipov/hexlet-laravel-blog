958ff012e844dc3592cf984b9d684d67d924baca
Tzsharipov <Tzsharipov@gmail.com>
2024-03-04 23:12:41 +0300

    new changes 04.03.2024

---
+++ app/Http/Controllers/MyFirstController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyFirstController extends Controller
+{
+    public function vase () {
+
+        return 'My project has new pages # 5 and # 10';     
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyFirstController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+ 
+    return view('welcome');    
+
+});
+Route::get('/my_page', [ MyFirstController::class, 'vase']);
+Route::get('about', function () {
+    return view('about');
 });
